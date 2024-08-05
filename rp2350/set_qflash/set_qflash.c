30b32be2a500443cacd42eabb379a1b58517d7ae
Kirk Benell <kirk.benell@sparkfun.com>
2024-08-05 13:57:50 -0600

    fixo of date on license

--- rp2350/set_qflash/set_qflash.c
+++ rp2350/set_qflash/set_qflash.c
@@ -9,7 +9,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (c) 2023 SparkFun Electronics
+Copyright (c) 2024 SparkFun Electronics
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
