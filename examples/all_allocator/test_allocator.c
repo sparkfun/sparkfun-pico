88f35736df319cdec412fb336793417e805d0af3
Kirk Benell <kirk.benell@sparkfun.com>
2024-08-19 14:31:36 -0600

    move to common include file; update all headers in file, test

--- examples/all_allocator/test_allocator.c
+++ examples/all_allocator/test_allocator.c
@@ -1,9 +1,34 @@
-/**
 
+/**
+ * @file test_allocator.c
+ *
+ * @brief This file contains a example on how to use a memory allocator with PSRAM and SRAM on a rp2350
+ *
  */
 
+/*
+The MIT License (MIT)
+
+Copyright (c) 2024 SparkFun Electronics
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions: The
+above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED
+"AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
 #include "pico/stdlib.h"
-#include "sparkfun_pico/sfe_pico_alloc.h"
+#include "sparkfun_pico/sfe_pico.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
